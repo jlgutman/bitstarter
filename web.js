abfa7ab11770c0be833c4ea667a4d1144891d476
Jose Luis Gutierrez <jlgutman@gmail.com>
2013-07-06 23:55:33 +0000

    web.js updated to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
+var html = fs.readFileSync('index.html','utf8');
+//console.log("HTML> "+html);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
